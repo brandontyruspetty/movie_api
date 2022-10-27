251ed31bd2663870d20cccd19971955411ade94a
brandontyruspetty <tyrus1974@gmail.com>
2022-10-27 15:45:23 -0700

    added error-handling middleware

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ app.get('/movies', (req, res) => {
 //added express.static to access documentation.html
 app.use(express.static('public'));
 
+//added error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke.');
+});
+
 //added listener
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
