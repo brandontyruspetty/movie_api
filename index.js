dbabd411165d22b6d36a777f0986f820c1ca0791
brandontyruspetty <tyrus1974@gmail.com>
2022-12-02 13:16:52 -0800

    changed mongoose connection to remote hosting

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/myNoirMovies', { useNewUrlParser: true, useUnifiedTopology: true });
-
+//mongoose.connect('mongodb://localhost:27017/myNoirMovies', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //added morgan for logging requests
 app.use(morgan('common'));
